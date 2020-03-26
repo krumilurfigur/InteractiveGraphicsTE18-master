885cef0ffe5d070051aceb18c9d49ee5f97aca89
krumilurfigur <gabrieloberg3@gmail.com>
2020-03-26 11:17:43 +0100

    hade problem hela tiden som vi lyckades fixa 5 min innan lektionen slutade

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * This is a class
+ * Created 2020-03-25
+ *
+ * @author Magnus Silverdal
+ */
+public class Main {
+    public static void main(String[] args) {
+        Graphics graphics = new Graphics(1024,800);
+        graphics.start();
+    }
+}
